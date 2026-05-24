3be6babfb7cdca0ee2d5ab20d31ffa27ab078f56
Ye Phone Kyaw <yephonekyaw920@gmail.com>
2026-05-24 02:23:14 -0600

    Create DTOs for User login

---
+++ backend/DTOs/AuthDto.cs
@@ -0,0 +1,27 @@
+namespace backend.DTOs;
+public class RegisterDto
+{
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+}
+
+public class LoginDto
+{
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+}
+
+public class UserResponseDto
+{
+    public int UserId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
+}
+
+public class AuthResponseDto
+{
+    public string Token { get; set; } = string.Empty;
+    public UserResponseDto User { get; set; } = null!;
+}
