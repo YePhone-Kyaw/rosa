cadbd3d80206efd191c9c72594bf89b377080a97
Ye Phone Kyaw <yephonekyaw920@gmail.com>
2026-06-30 00:07:33 -0600

    Update CategoryServiceTests.cs

--- backend.Tests/CategoryServiceTests.cs
+++ backend.Tests/CategoryServiceTests.cs
@@ -25,7 +25,7 @@ public class CategoryServiceTests
             "electronic.png"
         );
         var category = await db.Categories.FirstAsync();
-        
+
         return (categoryService, category);
     }
 
@@ -50,7 +50,7 @@ public class CategoryServiceTests
         Assert.Equal("Electronics", categories[0].CategoryName);
         Assert.Equal("electronic.png", categories[0].CategoryImageUrl);
     }
-    
+
     [Fact]
     public async Task GetCategoryById_ReturnCategoryData_WhenExists()
     {
@@ -69,5 +69,60 @@ public class CategoryServiceTests
         var category = await testData.categoryService.GetCategoryById(1234);
 
         Assert.Null(category);
+    }
+
+    [Fact]
+    public async Task CreateCategory_ReturnCategoryData_WhenSuccesses()
+    {
+        var db = GetInMemoryDb();
+        var categoryService = new CategoryService(db);
+
+        var category = await categoryService.CreateCategory(
+            new CreateCategoryDto { CategoryName = "Books" },
+            "books.png"
+        );
+
+        Assert.Equal("Books", category.CategoryName);
+        Assert.Equal("books.png", category.CategoryImageUrl);
+    }
+
+    [Fact]
+    public async Task UpdateCategory_ReturnUpdatedData_WhenSuccesses()
+    {
+        var testData = await CreateTestData();
+        var category = await testData.categoryService.UpdateCategory(testData.category.CategoryId,
+            new UpdateCategoryDto { CategoryName = "Books" });
+
+
+        Assert.NotNull(category);
+        Assert.Equal("Books", category.CategoryName);
+    }
+
+    [Fact]
+    public async Task UpdateCategory_ReturnNull_WhenNotExists()
+    {
+        var testData = await CreateTestData();
+        var category = await testData.categoryService.UpdateCategory(999,
+            new UpdateCategoryDto { CategoryName = "Books" });
+
+        Assert.Null(category);
+    }
+
+    [Fact]
+    public async Task DeleteCategory_ReturnTrue_WhenExists()
+    {
+        var testData = await CreateTestData();
+        var result = await testData.categoryService.DeleteCategory(testData.category.CategoryId);
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async Task DeleteCategory_ReturnFalse_WhenNotExists()
+    {
+        var testData = await CreateTestData();
+        var result = await testData.categoryService.DeleteCategory(999);
+
+        Assert.False(result);
     }
 }
